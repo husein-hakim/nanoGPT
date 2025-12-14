40fc14a636bc596ec3bbf306ace44c93bacf2901
Husein Hakim <husein@Huseins-MacBook-Pro.local>
2025-12-14 12:30:18 +0530

    updated vocab_size

--- GPTModel.py
+++ GPTModel.py
@@ -17,7 +17,8 @@ else:
 @dataclass
 class Config:
     block_size: int = 1024
-    vocab_size: int = 50257
+    # vocab_size: int = 50257
+    vocab_size: int = 32000
     n_layer: int = 12
     n_head: int = 12
     n_embd: int = 768
